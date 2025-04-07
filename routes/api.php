3daeeac94aeed7c01455104565a2e2a0e5f766d5
Islam Kotov <islam@Islams-MacBook-Pro.local>
2025-04-07 19:54:27 +0200

    User registration and login + api tokens

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+// Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('/auth/register', [UserController::class, 'createUser']);
+Route::post('/auth/login', [UserController::class, 'loginUser']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [UserController::class,'getProfile']);
+});
